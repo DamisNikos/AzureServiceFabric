595549759bcd28429e570eadf728de5c64a86bed
nikos <nikos@DESKTOP-26KBIDN>
2017-03-11 01:20:32 +0200

    arxidai

---
+++ CalculatorApplication/CalculatorClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+using CalculatorService;
+
+namespace CalculatorClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculatorClient = ServiceProxy.Create<ICalculatorService>
+(new Uri("fabric:/CalculatorApplication/CalculatorService"));
+            var result = calculatorClient.Add(1, 2).Result;
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
+
