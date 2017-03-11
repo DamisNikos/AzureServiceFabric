760d804cd6db325e60ab570cf001d5ea1b378917
nikos <nikos@DESKTOP-26KBIDN>
2017-03-11 05:26:52 +0200

    Done

---
+++ CalculatorApplication101/CalculatorClient101/Program.cs
@@ -0,0 +1,22 @@
+﻿using CalculatorService101;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorClient101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ICalculatorService calculatorClient = ServiceProxy.Create<ICalculatorService>(new Uri("fabric:/CalculatorApplication101/CalculatorService101"));
+            var result = calculatorClient.AddAsync(1, 2).Result;
+            Console.WriteLine(result);
+            Console.ReadLine();
+            
+        }
+    }
+}
